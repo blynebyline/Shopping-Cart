dd90dd4e631dd0aa5874ffb437f1528b5523bfce
Bryne <228299496+b1yn3@users.noreply.github.com>
2026-07-17 01:29:31 +0800

    feat: add and apply roboto font

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,11 @@ import Logo from "./Logo"
 
 const Header = () => {
     return (
-        <header className=" bg-zinc-100 px-4 py-2 flex">
-            <Logo></Logo>
+        <header className=" bg-zinc-100 px-4 py-2 flex font-roboto">
+            <div className="flex items-center">
+                <Logo></Logo>
+                <h2>Hexternal</h2>
+            </div>
         </header>
     )
 }
