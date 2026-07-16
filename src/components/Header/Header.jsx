5c9cba75cd9407bd98c4ef25de1da0fced7ce77f
Bryne <228299496+b1yn3@users.noreply.github.com>
2026-07-17 01:48:29 +0800

    feat: add temporary code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,15 @@ import Logo from "./Logo"
 
 const Header = () => {
     return (
-        <header className=" bg-zinc-100 px-4 py-2 flex font-roboto">
+        <header className=" bg-zinc-100 px-4 py-2 flex font-roboto justify-between items-center">
             <div className="flex items-center">
                 <Logo></Logo>
                 <h2>Hexternal</h2>
+            </div>
+            <div className="flex items-center gap-8 mx-10"> {/* MAKE IT SO THAT ONLY THIS WILL APPEAR ON MOBILE */}
+                <p>Home</p>
+                <p>Shop</p>
+                <p>Cart</p>
             </div>
         </header>
     )
