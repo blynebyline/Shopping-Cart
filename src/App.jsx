b928dab82082a011c23f074168b213e35dd48956
Bryne <228299496+b1yn3@users.noreply.github.com>
2026-07-18 09:44:02 +0800

    feat: connect files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './styles/App.css'
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header> </Header>
       <p className='text-cyan-400 text-center mt-5'>HELLO WORLD</p>
+      <Footer></Footer>
     </>
   )
 }
